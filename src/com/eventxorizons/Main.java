45c3f720f8f70b5293303f37fc6a649ae1fc9659
Stavros Pachoundakis <15883219+stavroschios@users.noreply.github.com>
2020-08-29 01:01:37 -0400

    Update Main.java

--- src/com/eventxorizons/Main.java
+++ src/com/eventxorizons/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+        locations.put(0, new Location(0, "You have escaped!",tempExit));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
